5fa3e8c8e8f6c89caa384ab12df0376a7a5c62c7
Sebastian <sebastian.sanhueza.t@usach.cl>
2026-03-05 16:16:22 -0300

    cuando abran la app varias veces, aparecerán la pestaña y solo habrá una instancia

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -116,6 +116,10 @@ pub fn run() {
         .plugin(tauri_plugin_single_instance::init(|app, argv, cwd| {
             println!("Se intentó abrir una nueva instancia con args: {:?}, cwd: {:?}", argv, cwd);
             let _ = app.emit("single-instance", ());
+            if let Some(window) = app.get_webview_window("main") {
+                let _ = window.show();
+                let _ = window.set_focus();
+            }
         }))
         .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_notification::init())
